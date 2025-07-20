e40cd2240027681b5148313930dbff22f8d90847
Catbug <duaahinai@gmail.com>
2025-07-20 10:24:11 +0400

    feat: initial project setup for ScientificCalculator

---
+++ src/ScientificCalculator.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ScientificCalculator {
+
+    public static void main(String[] args) {
+        // This is where your main calculator logic will go
+    }
+
+    public static void displayMenu() {
+        // This method will print the calculator menu options
+    }
+
+}
